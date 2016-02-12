f1c27ade104606ef1e5a903b7af3c7dce9bc2dd1
Roshan Pisharody <roshanpisharody1994@gmail.com>
2016-02-12 16:57:56 +0530

    First Version 0.01

--- app/src/main/java/com/thejoker/yts/UrlEndPoints.java
+++ app/src/main/java/com/thejoker/yts/UrlEndPoints.java
@@ -1,12 +1,12 @@
 package com.thejoker.yts;
 
-/**
- * Created by thejoker on 2/9/2016.
- */
+
 public class UrlEndPoints {
     public static  final String URL_MOVIE_DETAILS = "https://yts.ag/api/v2/movie_details.json";
+    public static  final String URL_MOVIE_LIST = "https://yts.ag/api/v2/list_movies.json";
     public  static final String URl_CHAR_QUESTION = "?";
     public static final String URl_CHAR_AMPERSAND = "&";
     public static final String URL_PARAM_ID = "movie_id=";
-   // public static final String URL_PARAM_PAGE = "page=";
+    public static final String URL_PARAM_PAGE = "page=";
+    public static final String URL_PARAM_LIMIT = "limit=";
 }
