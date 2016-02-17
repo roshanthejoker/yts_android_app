97d018c37ddd1634f5c85064d0d9cef2af8fdf6a
Roshan Pisharody <roshanpisharody1994@gmail.com>
2016-02-17 18:06:58 +0530

    First Version 0.01

--- app/src/main/java/com/thejoker/yts/Keys.java
+++ app/src/main/java/com/thejoker/yts/Keys.java
@@ -9,7 +9,16 @@ public interface Keys {
         String KEYS_TITLE_LONG = "title_long";
         String KEYS_RATING = "rating";
         String KEYS_DESCRIPTION = "description_full";
+        String KEY_YOUTUBE_ID = "yt_trailer_code";
         String KEYS_COVER = "small_cover_image";
         String KEYS_TORRENTS = "torrents";
+    }
+    interface EndPointMovieList{
+        String KEYS_MOVIES = "movies";
+        String KEYS_ID = "id";
+        String KEYS_TITLE = "title_long";
+        String KEYS_RATING ="rating";
+        String KEYS_YEAR = "year";
+        String KEYS_THUMBNAIL = "medium_cover_image";
     }
 }
