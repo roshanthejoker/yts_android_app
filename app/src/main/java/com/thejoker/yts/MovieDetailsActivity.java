07e4abc16c6b0fbdac94a974ca48702df4e15726
Roshan Pisharody <roshanpisharody1994@gmail.com>
2016-02-23 18:44:50 +0530

    First Version 0.01

--- app/src/main/java/com/thejoker/yts/MovieDetailsActivity.java
+++ app/src/main/java/com/thejoker/yts/MovieDetailsActivity.java
@@ -24,33 +24,57 @@ public class MovieDetailsActivity extends AppCompatActivity {
     private VolleySingleton volleySingleton;
     private RequestQueue mRequestQueue;
     private ArrayList<MovieDetails> detailsMovies = new ArrayList<>();
-    private ArrayList<MovieDetails> detailsUrl = new ArrayList<>();
+    private ArrayList<MovieDownloadDetails>downloadDetails = new ArrayList<>();
+    private String movieTitle ;
+    private String movieSummary ;
+    private int movieYear;
+    private long movieRating;
+    private String movieYoutubeId;
+    private String movieUrlThumbnail;
+    private String movieDownloadLink720p;
+    private String movieQuality720p;
+    private String moviefileSize720p;
+    private String movieDownloadLink1080p;
+    private String movieQuality1080p;
+    private String moviefileSize1080p;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
         Intent i = getIntent();
-        String movieId = i.getExtras().getString("movieId");
+        String movieIdString = i.getExtras().getString("movieId");
+        int movieId = Integer.parseInt(movieIdString);
         volleySingleton = VolleySingleton.getsInstance();
         mRequestQueue = VolleySingleton.getmRequestQueue();
-        parseMovieDetails();
+        parseMovieDetails(movieId,new VolleyCallback() {
+            @Override
+            public void onSuccess() {
+                movieYoutubeId = detailsMovies.get(0).getYoutube_id();
+                movieTitle = detailsMovies.get(0).getTitle();
+                movieUrlThumbnail=detailsMovies.get(0).getUrlThumbnail();
+                movieSummary = detailsMovies.get(0).getSummary();
+                movieYear = detailsMovies.get(0).getYear();
+                movieRating = detailsMovies.get(0).getRating();
+                movieQuality720p = downloadDetails.get(0).getQuality();
+                movieDownloadLink720p = downloadDetails.get(0).getDownloadLink();
+                moviefileSize720p = downloadDetails.get(0).getFileSize();
+                movieQuality1080p = downloadDetails.get(1).getQuality();
+                movieDownloadLink1080p = downloadDetails.get(1).getDownloadLink();
+                moviefileSize1080p = downloadDetails.get(1).getFileSize();
+            }
+        });
 
 
-
-//        Bundle b = new Bundle();
-//        b.putString("movie_Id", movieId);
-//        MovieDetailsFragment mdFragment = new MovieDetailsFragment();
-//        mdFragment.setArguments(b);
-//        FragmentManager manager = getSupportFragmentManager();
-//        FragmentTransaction ft = manager.beginTransaction();
-//        ft.replace(R.id.movie_detail_frame,mdFragment);
-//        ft.commit();
     }
-    public void parseMovieDetails(){
+
+
+    public void  parseMovieDetails(int id,final VolleyCallback callback){
 
 
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, getUrl(100), new Response.Listener<JSONObject>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, getUrl(id),new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 MovieDetails movieDetails = new MovieDetails();
@@ -63,30 +87,33 @@ public class MovieDetailsActivity extends AppCompatActivity {
                     JSONObject movieData = response.getJSONObject("data");
                     JSONObject movieDetailsObject = movieData.getJSONObject(Keys.EndPointMovieDetails.KEYS_MOVIE);
                     String movieTitle = movieDetailsObject.getString(Keys.EndPointMovieDetails.KEYS_TITLE_LONG);
-                    int movieRating = movieDetailsObject.getInt(Keys.EndPointMovieDetails.KEYS_RATING);
+                    long movieRating = movieDetailsObject.getInt(Keys.EndPointMovieDetails.KEYS_RATING);
                     String movieSynopsis = movieDetailsObject.getString(Keys.EndPointMovieDetails.KEYS_DESCRIPTION);
                     String moviePosterUrl = movieDetailsObject.getString(Keys.EndPointMovieDetails.KEYS_COVER);
+                    String movieYoutubeId = movieDetailsObject.getString(Keys.EndPointMovieDetails.KEY_YOUTUBE_ID);
                     movieDetails.setTitle(movieTitle);
                     movieDetails.setRating(movieRating);
                     movieDetails.setSummary(movieSynopsis);
                     movieDetails.setUrlThumbnail(moviePosterUrl);
+                    movieDetails.setYoutube_id(movieYoutubeId);
                     detailsMovies.add(movieDetails);
+
+
 
                     JSONArray torrentDownloadLinks = movieDetailsObject.getJSONArray(Keys.EndPointMovieDetails.KEYS_TORRENTS);
                     for(int i=0;i<torrentDownloadLinks.length();i++) {
+                        MovieDownloadDetails movieDownloadDetails = new MovieDownloadDetails();
                         JSONObject urlInfo = torrentDownloadLinks.getJSONObject(i);
                         String urlTorrent = urlInfo.getString("url");
                         String quality = urlInfo.getString("quality");
                         String fileSize = urlInfo.getString("size");
 
-                        movieDetails.setDownloadLink(urlTorrent);
-                        movieDetails.setQuality(quality);
-                        movieDetails.setFileSize(fileSize);
-                        detailsMovies.add(movieDetails);
+                        movieDownloadDetails.setDownloadLink(urlTorrent);
+                        movieDownloadDetails.setQuality(quality);
+                        movieDownloadDetails.setFileSize(fileSize);
+                        downloadDetails.add(movieDownloadDetails);
                     }
-                    Toast.makeText(getApplicationContext(),detailsMovies.toString(),Toast.LENGTH_LONG).show();
-
-
+                    callback.onSuccess();
 
 
 
@@ -95,6 +122,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
+
 
             }
 
@@ -106,12 +134,18 @@ public class MovieDetailsActivity extends AppCompatActivity {
         });
         mRequestQueue.add(jsonObjectRequest);
 
+
+
     }
 
     public String getUrl(int movieId){
         return UrlEndPoints.URL_MOVIE_DETAILS+
                 UrlEndPoints.URl_CHAR_QUESTION+
                 UrlEndPoints.URL_PARAM_ID+movieId;
+    }
+    public interface VolleyCallback {
+
+        void onSuccess();
     }
 }
 
