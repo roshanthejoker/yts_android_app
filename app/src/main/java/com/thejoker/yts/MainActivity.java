7e52db08752a22b5a990455e8cee39bfb3e46c4e
Roshan Pisharody <roshanpisharody1994@gmail.com>
2016-02-25 15:14:36 +0530

    First Version 0.01

--- app/src/main/java/com/thejoker/yts/MainActivity.java
+++ app/src/main/java/com/thejoker/yts/MainActivity.java
@@ -3,8 +3,10 @@ package com.thejoker.yts;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar appBar;
 
 
 
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_main);
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.movie_list_frame, new MovieListFragment());
